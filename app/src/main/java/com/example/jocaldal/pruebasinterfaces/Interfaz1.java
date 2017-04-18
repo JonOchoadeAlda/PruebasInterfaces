5306935aac135006e14ded0b7c21661693b06d62
Jon <j.ochoalda@gmail.com>
2017-04-18 13:00:07 +0200

    Ventana de inicio

---
+++ app/src/main/java/com/example/jocaldal/pruebasinterfaces/Interfaz1.java
@@ -0,0 +1,13 @@
+package com.example.jocaldal.pruebasinterfaces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Interfaz1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interfaz1);
+    }
+}
